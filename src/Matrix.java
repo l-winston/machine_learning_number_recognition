b371c81168f99cae08415e0b4d286ce075e04a71
Winston Liu <winwinstonliu@gmail.com>
2017-11-23 17:13:30 -0800

    message

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,7 +1,15 @@
+import javax.print.attribute.standard.PrinterLocation;
+
 public class Matrix {
 	int[][] matrix;
 	int columns;
 	int rows;
+
+	public static void main(String[] args) {
+		Matrix a = new Matrix(new int[][] { { 1, 2 }, { 3, 4 } });
+		Matrix b = new Matrix(new int[][] { { 2, 0 }, { 1, 2 } });
+		System.out.println(a.multiply(b));
+	}
 
 	public Matrix(int[][] matrix) {
 		this.matrix = matrix;
@@ -47,8 +55,37 @@ public class Matrix {
 	}
 
 	public Matrix multiply(Matrix m) {
+		if (columns != m.rows) {
+			throw new IllegalArgumentException("Matrix A.columns =/= Matrix B.rows");
+		}
+		int[][] ret = new int[this.rows][m.columns];
+		for (int i = 0; i < this.rows; i++) {
+			for (int j = 0; j < m.columns; j++) {
+				int x = 0;
+				for (int col = 0; col < this.columns; col++) {
+					for (int row = 0; row < m.rows; row++) {
+						x += matrix[i][col] * m.matrix[row][j];
+					}
+				}
+				ret[i][j] = x;
+			}
+		}
 
-		return null;
+		return new Matrix(ret);
+	}
+
+	public Matrix multiply(int scalar) {
+		int[][] ret = new int[this.rows][this.columns];
+		for (int i = 0; i < this.rows; i++) {
+			for (int j = 0; j < this.columns; j++) {
+				ret[i][j] *= scalar;
+			}
+		}
+		return new Matrix(ret);
+	}
+	
+	public Matrix subtract(Matrix m){
+		return this.add(m.multiply(-1));
 	}
 
 }
